f9528397a203ec414d5a95f1b161e5185b6d1bf6
Leslie Garcia <leslie.garcia2433@gmail.com>
2023-01-21 13:14:10 -0600

    Wrote schema file

---
+++ schema.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE IF NOT EXISTS employee_db;
+
+USE employee_db;
+
+CREATE TABLE IF NOT EXISTS department (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  dep_name VARCHAR(30) NOT NULL,
+);
+
+CREATE TABLE IF NOT EXISTS roles (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT,
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+  ON DELETE SET NULL
+);
+
+CREATE TABLE IF NOT EXISTS employee (
+  id INT NOT NULL PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT,
+  FOREIGN KEY (role_id)
+  REFERENCES roles(id)
+  ON DELETE SET NULL
+  manager_id INT,
+  FOREIGN KEY (manager_id)
+  REFERENCES roles(id)
+  ON DELETE SET NULL
+);
